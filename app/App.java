d90c169582c06a5f54d908f0da3cca5b84447971
Piekut <tobias@piekut.dk>
2020-10-07 12:28:45 +0200

    Udkommateret

--- app/App.java
+++ app/App.java
@@ -6,27 +6,27 @@ import java.awt.Color;
 
 public class App {
 	public static void main(String[] args) {
-		EdgeDetector eDetector = new EdgeDetector("app/images/small_sandwitch.jpg");
-		BufferedImage test = eDetector.getBufferedImage();
-		System.out.println(test);
 
-		int[][] testMag = eDetector.getMagnitudeArray();
-		for (int i = 0; i < testMag.length; i++) {
-			for (int j = 0; j < testMag[i].length; j++) {
+		Color[][] 
+		// EdgeDetector eDetector = new EdgeDetector("app/images/small_sandwitch.jpg");
+		// BufferedImage test = eDetector.getBufferedImage();
+		// System.out.println(test);
+
+		// int[][] testMag = eDetector.getMagnitudeArray();
+		// for (int i = 0; i < testMag.length; i++) {
+		// 	for (int j = 0; j < testMag[i].length; j++) {
 				
-				System.out.println(testMag[i][j]);
-			}
-		}
+		// 		System.out.println(testMag[i][j]);
+		// 	}
+		// }
 
-		Color[][] testGrey =  eDetector.getGreyscaleArray();
-		for (int i = 0; i < testGrey.length; i++) {
-			for (int j = 0; j < testGrey[i].length; j++){
+		// Color[][] testGrey =  eDetector.getGreyscaleArray();
+		// for (int i = 0; i < testGrey.length; i++) {
+		// 	for (int j = 0; j < testGrey[i].length; j++){
 
-				System.out.println(testGrey[i][j]);
-			}
-		}
-
-		
+		// 		System.out.println(testGrey[i][j]);
+		// 	}
+		// }
 
 
 	}
