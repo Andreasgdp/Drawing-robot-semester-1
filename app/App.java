aec18f2e37339e3d7662fd42f7a09c833d925de2
Piekut <tobias@piekut.dk>
2020-10-07 12:15:40 +0200

    GreyscaleArray

--- app/App.java
+++ app/App.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 
 public class App {
 	public static void main(String[] args) {
-		EdgeDetector eDetector = new EdgeDetector("app/images/black.jpg");
+		EdgeDetector eDetector = new EdgeDetector("app/images/small_sandwitch.jpg");
 		BufferedImage test = eDetector.getBufferedImage();
 		System.out.println(test);
 
@@ -16,8 +16,17 @@ public class App {
 				
 				System.out.println(testMag[i][j]);
 			}
-			
 		}
+
+		Color[][] testGrey =  eDetector.getGreyscaleArray();
+		for (int i = 0; i < testGrey.length; i++) {
+			for (int j = 0; j < testGrey[i].length; j++){
+
+				System.out.println(testGrey[i][j]);
+			}
+		}
+
+		
 
 
 	}
