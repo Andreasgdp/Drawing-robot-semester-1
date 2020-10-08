8e0cf474a6036fd2687d37fcf79401296cd9cad5
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2020-10-08 13:18:34 +0200

    Client and server

--- Server/Client.java
+++ Server/Client.java
@@ -3,18 +3,22 @@ import java.io.*;
 
 public class Client {
 	public static void main(String[] args) {
-		Socket s = new Socket("LocalHost", 4999);
-
-		PrintWriter pr = new PrintWriter(s.getOutputStream());
-
-		pr.println("it is working");
-		pr.flush();
-
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
-
-		String str = bf.readLine();
-		System.out.println("Server : " + str);
+		try {
+			Socket s = new Socket("10.0.0.48", 4999);
+			PrintWriter pr = new PrintWriter(s.getOutputStream());
+	
+			pr.println("it is working");
+			pr.flush();
+	
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
+	
+			String str = bf.readLine();
+			System.out.println("Server : " + str);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
 
 	}
 }
