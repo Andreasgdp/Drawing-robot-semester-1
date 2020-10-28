d2449e225aedc638b19b4dcddd0502959e8e50d5
Andreas <andreasgdp@gmail.com>
2020-10-28 20:39:57 +0100

    Added animated draw of sorted points

--- java_app/src/app/drawing_in_java/AnimatedDraw.java
+++ java_app/src/app/drawing_in_java/AnimatedDraw.java
@@ -2,24 +2,69 @@ package app.drawing_in_java;
 
 import app.edgedetect.Point;
 
-import java.awt.Color;
-import java.awt.*;
 import javax.swing.*;
-import java.util.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class AnimatedDraw extends JPanel {
-    private ArrayList<Point> points;
+    public AnimatedDraw(ArrayList<Point> points, int width, int height) {
+        EventQueue.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                ex.printStackTrace();
+            }
 
-    public AnimatedDraw(ArrayList<Point> points) {
-        this.points = points;
+            JFrame frame = new JFrame("Testing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new TestPane(points, width, height));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        this.setBackground(Color.WHITE);
-        g.setColor(Color.BLACK);
-        for (Point point : this.points) {
-            g.fillRect(point.x, point.y, 1, 1);
+    public static class TestPane extends JPanel {
+
+        ArrayList<Point> points;
+        int width;
+        int height;
+        int index = 0;
+
+
+        public TestPane(ArrayList<Point> points, int width, int height) {
+            this.points = points;
+            this.width = width;
+            this.height = height;
+            Timer timer = new Timer(3, e -> {
+                if (index < points.size()) {
+                    index++;
+                }
+                repaint();
+            });
+            timer.start();
         }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(this.width, this.height);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setColor(Color.RED);
+            for (int i = 0; i < index; i++) {
+                g.fillRect(points.get(i).x, points.get(i).y, 1, 1);
+            }
+            g2d.dispose();
+        }
+
     }
+
 }
+
+
+
+
